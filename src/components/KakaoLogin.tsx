369212426e62fc3230400134f0327a40304731e1
kim-0617 <kkk5993@naver.com>
2023-02-01 13:57:58 +0900

    kakao restapi token get

--- src/components/KakaoLogin.tsx
+++ src/components/KakaoLogin.tsx
@@ -1,6 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { REST_API_KEY, REDIRECT_URI } from "../utils/auth";
+import { useNavigate } from "react-router-dom";
+import { useAppDispatch } from "src/stores/hooks";
+import userSlice from "src/stores/reducers/user";
+import queryString from "query-string";
 
 function KakaoLogin() {
+  const params = queryString.parse(window.location.search);
+  const kakao_code = params?.code;
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
+  const getKakaoToken = () => {
+    fetch("https://kauth.kakao.com/oauth/token", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded",
+      },
+      body: `grant_type=authorization_code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&code=${kakao_code}`,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.access_token) {
+          dispatch(userSlice.actions.login({ token: data.access_token }));
+          window.Kakao.Auth.setAccessToken(data.access_token);
+          navigate("/");
+        } else {
+          navigate("/");
+        }
+      });
+  };
+
+  useEffect(() => {
+    if (!window.location.search) return;
+    getKakaoToken();
+  }, []);
+
   return <div>KakaoLogin</div>;
 }
 

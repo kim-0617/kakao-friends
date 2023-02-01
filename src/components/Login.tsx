6a4bfca6e352d0618a5f36a5ba65c4d0db0d14d0
kim-0617 <kkk5993@naver.com>
2023-02-01 14:01:09 +0900

    login 시도

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,8 +1,17 @@
-import React, { useEffect } from "react";
+import React from "react";
+import { REDIRECT_URI, REST_API_KEY } from "../utils/auth";
 
+const KAKAO_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=friends,talk_message`;
 function Login() {
-  useEffect(() => {}, []);
-  return <div>login</div>;
+  const handleLogin = () => {
+    window.location.href = KAKAO_URL;
+  };
+
+  return (
+    <div>
+      <button onClick={handleLogin}>카카오톡 로그인</button>
+    </div>
+  );
 }
 
 export default Login;

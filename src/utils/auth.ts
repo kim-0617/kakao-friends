820d97bf7157e494ba1180495af5b1bf135c87b6
kim-0617 <kkk5993@naver.com>
2023-02-01 13:58:32 +0900

    auth key add

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,3 +1,4 @@
 export const REST_API_KEY = "55776acba1e11f533bb29c5caa440f49";
 export const JAVASCRIPT_KEY = "198974679ddc1114f0b50ecd1aea13ab";
 export const REDIRECT_URI = "http://localhost:3000/kakaologin";
+export const APP_ADMIN_KEY = "d8c49463441a6289ea1e7580f88a3140";

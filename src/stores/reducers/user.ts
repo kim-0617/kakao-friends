daa41da26a7160b2acea7fd5d15f44905025e6e8
kim-0617 <kkk5993@naver.com>
2023-02-01 13:59:37 +0900

    getFriend, resetInfo 추가

--- src/stores/reducers/user.ts
+++ src/stores/reducers/user.ts
@@ -1,7 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState: { token: string } = {
+const initialState: { token: string; uuid: string[] } = {
   token: "",
+  uuid: [],
 };
 
 const userSlice = createSlice({
@@ -9,8 +10,19 @@ const userSlice = createSlice({
   initialState,
   reducers: {
     login(state, action) {
-      console.log("login", state, action);
+      console.log("login", action);
       state.token = action.payload.token;
+    },
+
+    getFriends(state, action) {
+      console.log("getFriends", action);
+      state.uuid = action.payload.uuid;
+    },
+
+    resetInfo(state, action) {
+      console.log("reset", action);
+      state.token = initialState.token;
+      state.uuid = initialState.uuid;
     },
   },
   extraReducers: {},
